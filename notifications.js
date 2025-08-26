e8e7437a0f577f92b8fc6fc71799d8804696c6aa
LouisJanquart <louis.janquart@gmail.com>
2025-08-26 15:53:23 +0200

    notifs

---
+++ notifications.js
@@ -0,0 +1,29 @@
+const notifications = () => {
+  const notifBtn = document.querySelector(".notify-me");
+  notifBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let myNotification = null;
+
+    const options = {
+      body: "Texte de la notif",
+      icon: "icons/favicon-16x16.png",
+      url: "https://cepegra.be",
+      vibrate: [200, 100, 200],
+    };
+    if (!("Notification" in window)) {
+      alert("Ce navigateur ne supporte pas les notifications");
+    } else if (Notification.permission === "granted") {
+      console.log("Notification permission already granted");
+      myNotification = new Notification("Hello", options);
+    } else {
+      Notification.requestPermission().then((result) => {
+        if (result === "granted") {
+          console.log("Notification permission granted");
+          myNotification = new Notification("Hello", options);
+        }
+      });
+    }
+  });
+};
+
+notifications();
