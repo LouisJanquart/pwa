4b03ba6831920a67412e059b9eaebec973d7282a
LouisJanquart <louis.janquart@gmail.com>
2025-08-26 09:19:13 +0200

    css + js

---
+++ main.js
@@ -0,0 +1,14 @@
+try {
+  const response = await fetch(
+    "https://ingrwf12.cepegra-frontend.xyz/cockpit1/api/content/items/voyages"
+  );
+  const data = await response.json();
+  document.querySelector(".voyage").textContent = data[0]["voyages-label"];
+  document.querySelector(".description").innerHTML =
+    data[0]["voyages-description"];
+  document
+    .querySelector(".prix")
+    .insertAdjacentText("afterbegin", `${data[0]["voyages-prix"]} `);
+} catch (error) {
+  console.error("Error fetching data:", error);
+}
